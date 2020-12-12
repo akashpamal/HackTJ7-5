019e60aa54b7b6535ea995214dea69c2d84e07c5
Akash Pamal <akashpamal@gmail.com>
2020-12-12 11:08:47 -0500

    first code commit

---
+++ transportation_hack_tj_7_5/android/app/src/main/java/com/akashpamal/transportation_hack_tj_7_5/MainActivity.java
@@ -0,0 +1,6 @@
+package com.akashpamal.transportation_hack_tj_7_5;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ transportation_hack_tj_7_5/lib/person.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Person {
+  String _starting_location;
+  String _ending_location;
+
+  Person(){
+    
+  }
+  
+  
+}
